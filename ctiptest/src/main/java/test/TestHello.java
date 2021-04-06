28cd3168dff8603892d1be915146d0862e823bc6
hongmon1 <hongmon1@naver.com>
2021-04-06 16:36:12 +0900

    redmine github integration test

--- ctiptest/src/main/java/test/TestHello.java
+++ ctiptest/src/main/java/test/TestHello.java
@@ -2,7 +2,8 @@ package test;
 
 public class TestHello {
     public static void main(String[] args){
-        System.out.println("Heloo world!");
+        System.out.println("Hello world!");
+        System.out.println("Test CTIP");
     }
 
 }
