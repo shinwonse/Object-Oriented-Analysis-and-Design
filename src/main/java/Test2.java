ac526e8bd40a7352998e0a77ed7987a3eb8e373b
hongmon1 <hongmon1@naver.com>
2021-04-30 15:03:45 +0900

    init

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -2,10 +2,6 @@ public class Test2 {
 
     public static void main(String[] args){
 
-        System.out.println("test");
-        System.out.println("test2");
-
-        System.out.println("test3");
     }
 
 }
