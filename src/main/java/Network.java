ff89ba16afbc1125b827a5880c21f5825db33cdb
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2021-05-25 19:06:47 +0900

    수정

--- src/main/java/Network.java
+++ src/main/java/Network.java
@@ -33,63 +33,10 @@ public class Network extends Thread {
             case MsgType.DRINK_SALE_CHECK:
                 int remainedStock = (int)handleSaleRequest(src_id, dst_id, msg);
                 return remainedStock;
+        }
         return null;
     }
 
-    private Object handleStockRequest(int src_id, int dst_id, String msg) {
-        ArrayList<DVM> accessibleDVMList = new ArrayList<>();
-        if(dst_id == 0){
-            for(DVM dvm : dvmList){
-                boolean isInStock = false;
-                for(Drink drink: dvm.getDrink_list()){
-                    if(drink.getName().equals(msg)){
-                        Message message = dvm.makeStockResponseMessage(src_id, drink.getStock()); // DVM이 응답 메세지를 만듬
-                        int stock = dvm.responseStockMessage(this, message); // DVM이 Network를 통해 응답메시지를 전달하고 Network 내부에서 stock 값을 찾아 리턴해줌
-                        if(stock != 0){
-                            isInStock = true;
-                            break;
-                        }
-                    }
-                }
-                if(isInStock){
-                    accessibleDVMList.add(dvm);
-                }
-            }
-        }
-        else{
-            for(DVM dvm : dvmList) {
-                if (dvm.getId() == dst_id){
-                    for (Drink drink : dvm.getDrink_list()) {
-                        if (drink.getName().equals(msg)) {
-                            Message message = dvm.makeStockResponseMessage(src_id, drink.getStock()); // DVM이 응답 메세지를 만듬
-                            int stock = dvm.responseStockMessage(this, message); // DVM이 Network를 통해 응답메시지를 전달하고 Network 내부에서 stock 값을 찾아 리턴해줌
-                            return stock;
-                        }
-                    }
-                }
-            }
-        }
-        return accessibleDVMList;
-    }
-
-    public int responseBroadcastMessage(Message message) {
-        int src_id = message.getSrc_id();
-        int dst_id = message.getDst_id();
-        String msg = message.getMsg();
-        int stock = Integer.parseInt(msg);
-
-        return stock;
-
-    }
-
-    public int responseNormalMessage(Message message) {
-        int data = -1;
-        int msg_type = message.getMsg_type();
-        String msg = message.getMsg();
-        if (msg_type == MsgType.RESPONSE_LOCATION) {
-            data = Integer.parseInt(msg);
-        }
-      
     // 판매 확인 요청
     private Object handleSaleRequest(int src_id, int dst_id, String msg) {
         int remainedStock = 0;
