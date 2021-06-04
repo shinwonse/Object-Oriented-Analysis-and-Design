290c8525f7f7af3dbc14fa41f3dd3efb1560731c
Park seoyoung <52600701+seoyoung-0@users.noreply.github.com>
2021-06-04 10:19:25 +0900

    Add setCodeTest in CodeTest

--- src/main/java/Code.java
+++ src/main/java/Code.java
@@ -11,11 +11,9 @@ public class Code {
     public int getCode() {
         return code;
     }
-
     public void setCode(int code) {
         this.code = code;
     }
-
     public Drink getDrink() {
         return drink;
     }
